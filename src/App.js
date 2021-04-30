49321a116665f3398ce8c973079735b773fa92cb
NareshRobertson <naresh.nagandla@robertsoncollege.ca>
2021-04-29 19:30:20 -0500

    Changed the text message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div> This is my new div</div>
+      <div> Deploying this app to Azure service</div>
     </div>
   );
 }
