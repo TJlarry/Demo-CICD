61d4d0bf92976fdc71c6f46bd0c29e772a81e8ce
NareshRobertson <naresh.nagandla@robertsoncollege.ca>
2021-04-29 20:12:31 -0500

    Added a new enhancement

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div> Deploying this app to Azure service</div>
+      <div> Deploying a new enhancement to Azure service</div>
     </div>
   );
 }
