89377d7bba2d04a17883f41a95881ef1f59dbd89
NareshRobertson <naresh.nagandla@robertsoncollege.ca>
2021-08-24 14:36:01 -0700

    Changes made by Naresh for start of Jan 2021 Batch

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ function App() {
   return (
     <div className="App">
       <div> This is a code change from Naresh</div>
-      <div> This is a code from Karolina</div>
-      <div>This is another update from the real Karolina</div>
-      <div>Kadar was here</div>
-      <div>This is a code from Rail</div>
-      <div>Code by Tina</div>
-      <div>This update is brought to you by Luke</div>
     </div>
   );
 }
