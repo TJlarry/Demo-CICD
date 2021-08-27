04c75d0615b214245a56075ab5bbad29d57cb7d0
Edwin Rico Carandang <edwinrn.carandang@yahoo.com>
2021-08-27 01:00:10 -0500

    Updated comment by Edwin

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div> This is a code change from Naresh</div>
-      <div>Test - Edwin</div>
+      <div>This is a code change from Naresh</div>
+      <div>Module 3 Assignment - Cloud Deployment with Azure | Edwin Rico Carandang</div>
     </div>
   );
 }
