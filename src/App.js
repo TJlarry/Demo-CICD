aff748eadbf53a0657e0aed525c8017b3a12c089
calebpendev <calebpendev@gmail.com>
2021-08-29 07:35:39 -0700

    Added my name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>This is a code change from Caleb Penman</div>
+      <div>This is a code change from Caleb</div>
       <div>Module 3 Assignment - Cloud Deployment with Azure | E.R. Carandang</div>
       <div>This is a code change from Michael</div>
     </div>
