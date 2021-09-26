035fa2c9c691f86f82c93b60ca1c394cf749ee5d
aarontumambing <aaron.tumambing6@gmail.com>
2021-09-26 09:16:42 -0500

    Commit by Aaron Tumambing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <div>This is a code change from Delphine Nwawuzie</div>
+      <div>Code added by Aaron Tumambing</div>
     </div>
   );
 }
