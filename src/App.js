ecd66906676926960b2936b773a6476466163e41
NareshRobertson <naresh.nagandla@robertsoncollege.ca>
2021-11-15 20:43:15 -0600

    Change Done on Nov 15th 2021

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>This is a code change from Delphine Nwawuzie</div>
-      <div>Code added by Aaron Tumambing</div>
-      <div>Code added by Adam Pitcher</div>
-      <div>Code added by Francis Ryan Pasicolan</div>
+      <div>This is a code change from Naresh Nagandla</div>
     </div>
   );
 }
