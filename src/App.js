8b1b3ee489cc0cf9630f076036995e1b33ec2324
TJlarry <alliokelanre@gmail.com>
2021-12-15 20:20:28 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <div>This is a code change from Naresh Nagandla</div>
+      <div> learning deployment of application</div>
+      <div>  project </div>
     </div>
   );
 }
